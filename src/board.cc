9584df06780468160a93083a3ba70d05c00f8101
ambition_echo <ambition_echo@outlook.com>
2022-03-24 21:17:18 +0800

    feat: 获取内存使用率

--- src/board.cc
+++ src/board.cc
@@ -1,11 +1,10 @@
 #include "board.hh"
 #include "ui_board.h"
 
-Board::Board(QWidget *parent)
-    : QWidget(parent)
-    , ui(new Ui::Board)
+Board::Board(QWidget *parent) : QWidget(parent), ui(new Ui::Board)
 {
     ui->setupUi(this);
+    init();
 }
 
 Board::~Board()
@@ -13,3 +12,40 @@ Board::~Board()
     delete ui;
 }
 
+//初始化
+void Board::init()
+{
+    timer.start(1000);
+    connect(&timer, &QTimer::timeout, this, &Board::memRefresh);
+}
+
+//获取内存信息
+int Board::getMem()
+{
+    QFile memInfo("/proc/meminfo");
+    memInfo.open(QIODevice::ReadOnly | QIODevice::Text);
+
+    QString memStr;
+    QStringList memStrList;
+    memStr = memInfo.readAll();
+    memInfo.close();
+    memStrList = memStr.split('\n');
+    memStrList[0].remove(' ');
+    memStrList[0].remove("MemTotal:");
+    memStrList[0].remove("kB");
+    this->memTotal = memStrList[0].toInt();
+
+    memStrList[2].remove(' ');
+    memStrList[2].remove("MemAvailable:");
+    memStrList[2].remove("kB");
+    this->memAvailable = memStrList[2].toInt();
+
+    return (((this->memTotal - this->memAvailable) * 100) / this->memTotal);
+}
+
+//刷新内存信息
+void Board::memRefresh()
+{
+    ui->memPercentLabel->setNum(getMem());
+    timer.start(1000);
+}
--- src/board.hh
+++ src/board.hh
@@ -1,19 +1,32 @@
 #pragma once
 
+#include <QFile>
+#include <QTimer>
 #include <QWidget>
-
 QT_BEGIN_NAMESPACE
-namespace Ui { class Board; }
+namespace Ui
+{
+class Board;
+}
 QT_END_NAMESPACE
 
 class Board : public QWidget
 {
     Q_OBJECT
 
-public:
+  public:
+    int memAvailable; //可用内存
+    int memTotal;     //总内存
+    int cpu;
+    QTimer timer; //定时器
+
     Board(QWidget *parent = nullptr);
     ~Board();
 
-private:
+    int getMem();
+    void memRefresh();
+    void init();
+
+  private:
     Ui::Board *ui;
 };
